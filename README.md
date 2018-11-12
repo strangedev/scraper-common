fe86083d05e4f2c9ad6bee16aa8f308167b024ab
Noah Hummel <n.p.hummel@gmail.com>
2018-11-12 16:52:40 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # scraper-common
 Common files and library foo for web scraping
+
+## Install requirements
+
+```bash
+pip install -r requirements.txt
+```
+
+## Run tests
+
+```bash
+python -m unittest
+```
---
+++ test/lib/test_singleton.py
@@ -0,0 +1,11 @@
+import unittest
+
+from src.lib.singleton import SingletonMeta
+
+
+class TestSingletonMeta(unittest.TestCase):
+    def test_is_singleton_unique(self):
+        class TestSingleton(metaclass=SingletonMeta):
+            pass
+
+        self.assertIs(TestSingleton(), TestSingleton(), "Singleton was created multiple times.")
