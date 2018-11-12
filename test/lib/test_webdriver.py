b68639d3bd46abea4d6fa75354d2e16a3ac75c64
Noah Hummel <n.p.hummel@gmail.com>
2018-11-12 17:14:12 +0100

    [FIX] missing files due to gitignore

---
+++ test/lib/test_webdriver.py
@@ -0,0 +1,17 @@
+import unittest
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+from src.lib.webdriver import WebDriver
+
+
+class TestWebdriver(unittest.TestCase):
+    def test_webdriver_is_configured(self):
+        options = Options()
+        options.headless = True
+        driver = webdriver.Chrome(options=options)
+        driver.close()
+
+    def test_create_new_webdriver(self):
+        self.assertIsNotNone(WebDriver().d)
